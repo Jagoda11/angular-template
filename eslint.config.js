19ed3588e2cdeb57a98de7d3b56a5f1370c8e124
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-11-12 19:55:34 +0100

    📝 Update package.json and package-lock.json for license information; fix ESLint configuration and minor code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,8 @@
-// @ts-check
-import js from "@eslint/js";
-import ts from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 import angular from "@angular-eslint/eslint-plugin";
 import angularTemplate from "@angular-eslint/eslint-plugin-template";
 import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
@@ -17,35 +14,17 @@ export default [
       },
     },
     plugins: {
-      "@typescript-eslint": ts,
+      "@typescript-eslint": tsPlugin,
       "@angular-eslint": angular,
       prettier,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...ts.configs["recommended"].rules,
-      ...ts.configs["stylistic"].rules,
-      ...angular.configs["recommended"].rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...angular.configs.recommended.rules,
       "prettier/prettier": "error",
     },
   },
-  {
-    files: ["**/*.html"],
-    plugins: {
-      "@angular-eslint/template": angularTemplate,
-      prettier,
-    },
-    rules: {
-      ...angularTemplate.configs["recommended"].rules,
-      "prettier/prettier": [
-        "error",
-        {
-          parser: "angular",
-        },
-      ],
-    },
-  },
-  // Global ignores for all files and blocks
+
   {
     ignores: ["dist/", "node_modules/", "build/", "**/*.spec.ts"],
   },
