85972a5776e34fc53c2089067cce77656f5227f2
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-11-14 19:54:20 +0100

    📝 Refactor ESLint configuration and Angular component tests for consistency and improved readability

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,37 @@
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import angular from "@angular-eslint/eslint-plugin";
-import angularTemplate from "@angular-eslint/eslint-plugin-template";
-import prettier from "eslint-plugin-prettier";
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import angular from '@angular-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
     },
     plugins: {
-      "@typescript-eslint": tsPlugin,
-      "@angular-eslint": angular,
+      '@typescript-eslint': tsPlugin,
+      '@angular-eslint': angular,
       prettier,
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
       ...angular.configs.recommended.rules,
-      "prettier/prettier": "error",
+      'prettier/prettier': 'error',
     },
   },
 
   {
-    ignores: ["dist/", "node_modules/", "build/", "**/*.spec.ts"],
+    ignores: [
+      'dist/',
+      'node_modules/',
+      'build/',
+      '**/*.spec.ts',
+      'coverage/',
+      'karma.conf.js',
+    ],
   },
-];
+]
